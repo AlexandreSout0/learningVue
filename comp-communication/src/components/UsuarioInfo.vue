d1d2903e315c8472de9483923aa645174dd215f5
AlexandreSout0 <soutoinf@gmail.com>
2023-12-20 18:50:35 -0300

    Ok

---
+++ comp-communication/src/components/UsuarioInfo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <!-- <p>Nome do Usuário: <strong> {{ nome }} </strong></p> -->
+        <p>Nome do Usuário Invertido: <strong> {{ inverteNome() }} </strong></p>
+        <p>Idade do usuário <strong>{{ idade }} </strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome CallBack</button>
+
+    </div>
+</template>
+
+<script>
+//import barramento from '@/barramento'
+
+export default {
+   //props: ['nome'],
+   props: {
+        nome: {
+            type: String,
+            default: 'anônimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+   },
+   methods: {
+    inverteNome() {
+        return this.nome.split('').reverse().join('')
+    },
+
+    reiniciarNome() {
+        const antigo = this.nome;
+        const novoNome = 'Alexandre';
+        this.$emit('nomeMudou', { novoNome, antigo});
+    }   
+   },
+//    created() {
+//     barramento.$on('idadeMudou', idade => {
+//         console.log('A idade mudou para:', idade);
+//     })
+//    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
---
+++ comp-communication/src/components/UsuarioMain.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{ nome }}</strong></p>
+        <p>Idade <strong>{{ idade }}</strong></p>
+
+        <button @click="alterarNome"> Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+            <app-UsuarioInfo :nome="nome" :idade="idade" @nomeMudou="nome = $event.novoNome + $event.antigo" :reiniciarFn="reiniciarNome2callbak"/>
+            <app-AppUsuarioEditar :idade="idade" @idadeMudou="idade = $event" />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { 'app-UsuarioInfo': AppUsuarioInfo, 'app-AppUsuarioEditar': AppUsuarioEditar },
+    data() {
+        return {
+            nome: 'Alexandre',
+            idade: 31
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = 'Souto'
+        },
+
+        reiniciarNome2callbak() {
+            this.nome = "Alexandre"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
